61aee13eb9f4eb43bae03283f1c3aeaaad2739ac
Sweet_Cloud <45199326+sweet1cloud@users.noreply.github.com>
2025-05-18 14:27:50 +0900

    Add files via upload

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>YouTube Playlist Timer</title>
+      <meta name="description" content="유튜브 재생목록 재생시간 계산기" />
+    </>
+  )
+}
