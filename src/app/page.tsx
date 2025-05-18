09716b744bf36ff5868f9a9fcfeca6f011a2d303
Sweet_Cloud <45199326+sweet1cloud@users.noreply.github.com>
2025-05-18 13:47:08 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
 'use client'
 
 import { useState } from 'react'
